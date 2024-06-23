b01344a75c0382afb2f88fc949809692e0afe326
ArpanaMishra94 <arpanamishra394@gmail.com>
2024-06-23 21:28:55 +0530

    responsive navbar update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from "../utils/assets/logo.png";
+import React, { useState } from 'react';
+import logo from "../assets/logo.png";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
+    const navItems = [
+        { id: 1, text: 'Cart', icon: <FaShoppingCart size={25} color='white' />, route: '/cart' },
+        { id: 2, text: 'Profile', icon: <FaUser size={25} color='white' />, route: '/' },
+    ];
 
     return (
-        <header className='bg-gray-400 sticky top-0 z-[1] mx-auto flex w-full items-center justify-between flex-wrap p-5'>
-            <Link to = '/' className='flex items-center cursor-pointer'>
+        <header className='bg-[#7b8a8b] flex justify-between items-center h-24 mx-auto px-4 text-white'>
+            <Link to='/' className='flex items-center cursor-pointer'>
                 <img src={logo} alt="indiTrendz" />
-                <span className='text-white text-xl font-bold'>Indi trendz</span>
+                <h1 className='text-white w-full text-3xl font-bold'>Indi trendz</h1>
             </Link>
-                <div className='flex items-center justify-end mr-9 gap-8 overflow-hidden'>
-                    <Link to = '/cart' className='flex items-center gap-2 cursor-pointer'>
-                        <FaShoppingCart size={25} color='white' />
-                        <span className='text-white'>Cart</span>
+
+             {/* Desktop Navigation */}
+            <ul className='hidden md:flex'>
+                {navItems.map(item => (
+                    <Link to={item.route}>
+                        <li
+                            key={item.id}
+                            className='p-4 hover:bg-[#b5c0c1] flex gap-1 rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+                        >
+                            {item.icon}
+                            {item.text}
+                        </li>
                     </Link>
-                    <div className='flex items-center gap-2'>
-                        <FaUser size={25} color='white' />
-                        <span className='text-white'>Arpana</span>
-                    </div>
-                </div>
+                ))}
+            </ul>
+
+             {/* Mobile Navigation */}
+
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+
+            <ul
+                className={
+                    nav
+                        ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#7b8a8b] ease-in-out duration-500'
+                        : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+                    }
+                >
+                <h1 className='w-full text-3xl font-bold text-white m-4'>Cart</h1>
+
+                {navItems.map(item => (
+                    <Link to={item.route}>
+                        <li
+                            key={item.id}
+                            className='p-4 flex gap-1 border-b rounded-xl hover:bg-[#b5c0c1] duration-300 hover:text-black cursor-pointer border-gray-600'
+                        >
+                            {item.icon}
+                            {item.text}
+                        </li>
+                    </Link>
+                ))}
+            </ul>
         </header>
     )
 }
