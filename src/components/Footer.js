c6658f2c2f6497d9305b79558e1e6fc4211353ec
ArpanaMishra94 <arpanamishra394@gmail.com>
2024-06-28 23:20:49 +0530

    footer ui update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="container mt-8 mx-auto border-t border-[#7b8a8b] p-4 text-center text-[#505a5b]">
+        <footer className="container mx-auto border-t border-[#7b8a8b] p-4 text-center text-[#505a5b]">
             Copyright © ${new Date().getFullYear()} - All right reserved by Indi Trendz Industries Ltd
         </footer>
     )
